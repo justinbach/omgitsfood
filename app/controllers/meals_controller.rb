e8957b346fe6a30362b21a23edbf0b54bbe14acc
Justin Bachorik <justinbach@gmail.com>
2010-04-17 14:45:18 -0400

    started working on meals controller

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,17 @@
+class MealsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+end
--- test/functional/meals_controller_test.rb
+++ test/functional/meals_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class IngredientshipTest < ActiveSupport::TestCase
+class MealsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
