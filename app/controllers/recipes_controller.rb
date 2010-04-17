0fef27360673aded586d9df5be4ab55dbbd97e47
Justin Bachorik <justinbach@gmail.com>
2010-04-17 12:47:34 -0400

    got haml installed and working

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,5 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all(:order => "created_at DESC")
-    respond_to do |format|
-      format.html
-    end
   end
 end
