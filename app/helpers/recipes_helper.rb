fd3a47afc8a387409c62c00cd108c50941400dea
Justin Bachorik <justinbach@gmail.com>
2010-04-27 21:18:05 -0400

    minor tweak

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,7 +1,7 @@
 module RecipesHelper
   def edit_recipe_link_display
     if @recipe.user == current_user
-      link_to "Edit this recipe", :action => "edit", :id => @recipe
+      link_to "Edit this recipe", {:action => "edit", :id => @recipe}, :class => "utilityLink"
     end
   end
 end
