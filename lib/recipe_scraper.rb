207b8dc8151f2c5c80e91358668e7d040760c096
Justin Bachorik <justinbach@gmail.com>
2010-05-01 16:48:05 -0400

    cooks.com code in place but there's a bug

--- lib/recipe_scraper.rb
+++ lib/recipe_scraper.rb
@@ -4,58 +4,71 @@ require 'hpricot'
 require 'htmlentities'
 
 module RecipeScraper
-  def recipe_of_link
-  end
   
-  def load_recipe url
-    recipe = open(url) { |f| Hpricot(f) }
-    @recipe = Recipe.new
-    @recipe.title = get_title recipe
-    @recipe.ingredients = get_ingredients recipe
-    @recipe.directions = get_directions recipe
-    @recipe.yield = get_yield recipe
-    if @recipe.title == '' || @recipe.ingredients == '' || @recipe.directions == ''
-      nil
+  # using a factory pattern-ish design here
+  # ruby is duck-typed so interfaces / abstract base classes aren't used
+  def load_recipe url 
+    if !url.match(/epicurious.com/)
+      EpicuriousScraper.new.load_recipe url
+    #elsif !url.match(/cooks.com/)
+     # CooksScraper.new.load_recipe url
     else
-      @recipe
+      nil
     end
   end
+
   
-  def get_title recipe
-    title = ''
-    (recipe/"div#headline").each do |dh|
-      title = get_sanitized_innerHTML(dh.at("h1"))
-    end
-    title
-  end
-  
-  def get_ingredients recipe
-    ingredients = ''
-    (recipe/"ul.ingredientsList").each do |il|
-      (il/"li").each do |i|
-        ingredients << get_sanitized_innerHTML(i) + "\n"
-      end
-    end 
-    ingredients
-  end
-  
-  def get_directions recipe
-    directions = ''
-    (recipe/"div#preparation").each do |dp|
-      (dp/"p").each do |p|
-        directions << get_sanitized_innerHTML(p) + "\n\n"
+  # for scraping from Epicurious
+  class EpicuriousScraper
+    def load_recipe url
+      recipe = open(url) { |f| Hpricot(f) }
+      @recipe = Recipe.new
+      @recipe.title = get_title recipe
+      @recipe.ingredients = get_ingredients recipe
+      @recipe.directions = get_directions recipe
+      @recipe.yield = get_yield recipe
+      if @recipe.title == '' || @recipe.ingredients == '' || @recipe.directions == ''
+        nil
+      else
+        @recipe
       end
     end
-    directions
+  
+    def get_title recipe
+      title = ''
+      (recipe/"div#headline").each do |dh|
+        title = get_sanitized_innerHTML(dh.at("h1"))
+      end
+      title
+    end
+  
+    def get_ingredients recipe
+      ingredients = ''
+      (recipe/"ul.ingredientsList").each do |il|
+        (il/"li").each do |i|
+          ingredients << get_sanitized_innerHTML(i) + "\n"
+        end
+      end 
+      ingredients
+    end
+  
+    def get_directions recipe
+      directions = ''
+      (recipe/"div#preparation").each do |dp|
+        (dp/"p").each do |p|
+          directions << get_sanitized_innerHTML(p) + "\n\n"
+        end
+      end
+      directions
+    end
+  
+    def get_yield recipe
+      get_sanitized_innerHTML(recipe.at('span.yield'))
+    end
+  
+    def get_sanitized_innerHTML e
+      decoder = HTMLEntities.new
+      decoder.decode(e.innerHTML.to_s.strip.gsub(/<\/?[^>]*>/, ""))
+    end  
   end
-  
-  def get_yield recipe
-    get_sanitized_innerHTML(recipe.at('span.yield'))
-  end
-  
-  def get_sanitized_innerHTML e
-    decoder = HTMLEntities.new
-    decoder.decode(e.innerHTML.to_s.strip.gsub(/<\/?[^>]*>/, ""))
-  end  
-  
 end
