c545e726f5af01fde0339fc8facc8ec571f515d3
Justin Bachorik <justinbach@gmail.com>
2010-05-08 18:52:32 -0400

    added active statest back to nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,18 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-
+  
   def showFlash
     if flash[:notice]
       "<div id='flash'>#{flash[:notice]}</div>"
     end
   end
+  
+  def navLink text, path, controller 
+    if params[:controller] == controller
+      link_to text, path, :class => 'navActive'
+    else
+      link_to text, path
+    end
+  end
+  
 end
