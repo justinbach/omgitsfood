2bb902b66a037786c7488cc456aa86cb86f5dd39
Justin Bachorik <justinbach@gmail.com>
2010-05-09 09:41:50 -0400

    review editing functionality now in place

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,5 +14,13 @@ class ReviewsController < ApplicationController
   end
   
   def update
+    @review = Review.find(params[:id])
+    if @review.update_attributes(params[:review])
+      flash[:notice] = 'Review successfully updated.'
+      redirect_to user_meal_path(@review.user_id, @review.meal_id)
+    else
+      flash[:notice] = "There was a problem updating the review."
+      redirect_to user_meal_path(@review.user_id, @review.meal_id)
+    end
   end
 end
--- public/javascripts/meal_show.js
+++ public/javascripts/meal_show.js
@@ -5,4 +5,25 @@ $(document).ready(function() {
     $("#meal_day").focus();
   });
   $("#meal_day").bind("change", function() { $('form.edit_meal').submit(); });
+  
+  // autogrow review form
+  setUpAutogrow();
+  
+  // autogrow text areas
+  function setUpAutogrow() {
+    $('textarea').autogrow({
+      minHeight: '100px',
+      maxHeight: '500px'
+    });
+  }
+  
+  // logic for hiding/showing review form
+  $("#editReviewLink").click(function() {
+    $("#review").fadeOut('300', function() {
+      $('#editReview').css('display','block');
+      $('#editReview').fadeIn('300');
+    })
+    return false;
+  });
+  
 });
